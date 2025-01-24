5bdde0619a0ad0d5d172b5c780ab011bb8df5eaa
HayateOhsawa <hayateoosawa3150@gmail.com>
2025-01-24 12:21:20 +0900

    プロトタイプの編集・削除の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, :email, :profile, :occupation, :position, presence: true
+
+  has_many :prototypes
+  has_many :comments
 end
